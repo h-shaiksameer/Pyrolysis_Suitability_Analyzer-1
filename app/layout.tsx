2c354e08e78f84f9f1c2de33ff834eeb25799869
ShaikSameerHussain2104 <shaiksameerhussain2104@gmail.com>
2025-05-27 22:13:47 +0530

    ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
+    
     <html lang="en">
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="dark">
